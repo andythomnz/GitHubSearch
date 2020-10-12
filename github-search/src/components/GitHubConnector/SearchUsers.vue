60fadc47f247347fb4d80f5a0dddf9fa8a172305
Andrew Thompson <andythomnz@gmail.com>
2020-10-13 12:00:26 +1300

    Adding mock SearchUser GitHubConnector and blank UserResults view

---
+++ github-search/src/components/GitHubConnector/SearchUsers.vue
@@ -0,0 +1,48 @@
+<script>
+export async function search(searchTerm, sortBy, perPage, page) {
+  //check args are not undefined
+
+  let useArgs = `Search Term: ${searchTerm}, Sort: ${sortBy}, PerPage: ${perPage}, Page: ${page}`;
+  console.log(useArgs);
+  let response = await requestFromEndpoint();
+  let users = normalizeData(response.data.items);
+  return users;
+}
+
+async function requestFromEndpoint() {
+  return {
+    data: {
+      total_count: 1,
+      incomplete_results: false,
+      items: [
+        {
+          login: "andythomnz",
+          html_url: "https://github.com/users/andythomnz",
+          something: "somethingElse",
+        },
+      ],
+    },
+  };
+}
+
+function normalizeData(data) {
+  return data.map(normalizeUser);
+}
+
+function normalizeUser(user) {
+  let { login, html_url } = user;
+  return {
+    login,
+    html_url,
+  };
+}
+
+export default {
+  name: "SearchUsers",
+  data() {
+    return {
+      foo: "bar",
+    };
+  },
+};
+</script>
---
+++ github-search/src/views/UserResults.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="user-search-results">
+    <h1>Users Search Results</h1>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import { search } from "@/components/GitHubConnector/SearchUsers.vue";
+
+export default {
+  name: "UserResults",
+  data() {
+    return {
+      foo: "bar",
+    };
+  },
+  components: {},
+  created: function () {
+    this.refreshData();
+  },
+  methods: {
+    refreshData: function () {
+      search();
+    },
+  },
+};
+</script>
