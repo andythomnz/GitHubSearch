c66e6c947934b4124d263b3b9b3cbc45e876606a
Andrew Thompson <andythomnz@gmail.com>
2020-10-13 08:49:57 +1300

    Adding basic header component to app

--- github-search/src/App.vue
+++ github-search/src/App.vue
@@ -1,12 +1,30 @@
 <template>
   <div id="app">
+    <div id="header">
+      <Header />
+    </div>
     <div id="nav">
-      <router-link to="/">Home</router-link> |
+      <router-link to="/">Home</router-link>|
       <router-link to="/about">About</router-link>
     </div>
-    <router-view/>
+    <router-view />
   </div>
 </template>
+
+<script>
+// @ is an alias to /src
+import Header from '@/components/Header.vue'
+
+export default {
+  name: 'App',
+  created () {
+    document.title = "GitHub Search";
+  },
+  components: {
+    Header
+  }
+}
+</script>
 
 <style>
 #app {
---
+++ github-search/src/components/Header.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="header">
+    <img alt="GitHub logo" src="../assets/github.png" />
+    <h1>GitHub Search</h1>
+    <img alt="Search logo" src="../assets/search.png" />
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Header",
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+a {
+  color: #42b983;
+}
+.header {
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: space-evenly;
+  align-items: center;
+}
+</style>
